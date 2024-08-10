3076c271f7d0a126d7ccc064b912da000b72cbb0
Alan Zhu <2025azhu@tjhsst.edu>
2024-08-10 13:28:14 -0400

    Add additional fields to project admin

--- qlab/apps/projects/admin.py
+++ qlab/apps/projects/admin.py
@@ -9,6 +9,12 @@ def mark_inactive(modeladmin, request, queryset):
 
 
 class ProjectAdmin(admin.ModelAdmin):
+    def project_year(obj):
+        return obj.authors.first().graduation_year
+
+    list_display = ("title", "author_string", project_year, "last_updated", "active")
+    list_filter = ("active", "last_updated")
+    search_fields = ("title", "description", "authors__first_name", "authors__last_name")
     actions = [mark_inactive]
 
 
